61de4fb08c784be07550ed9a0661fda532c7ecd1
cjihrig <cjihrig@gmail.com>
2019-09-16 13:32:00 -0400

    wasi: add integration tests

---
+++ test/wasi/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+for file in c/*.c; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  /opt/wasi-sdk/bin/clang $file -target wasm32-unknown-wasi -o wasm/$filename.wasm
+done
