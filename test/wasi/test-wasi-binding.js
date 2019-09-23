401a92aa4a6f4b78f959fb0270d415253b426418
cjihrig <cjihrig@gmail.com>
2019-09-22 23:01:08 -0400

    implement fd_write()

--- test/wasi/test-wasi-binding.js
+++ test/wasi/test-wasi-binding.js
@@ -6,10 +6,7 @@ const assert = require('assert');
 const fixtures = require('../common/fixtures');
 const buffer = fixtures.readSync(['wasi', 'simple-wasi.wasm']);
 const { WASI } = require('wasi');
-
-const memory = new WebAssembly.Memory({ initial: 1 });
-const wasi = new WASI({ args: [], env: process.env, memory });
-
+const wasi = new WASI({ args: [], env: process.env });
 const importObject = {
   wasi_unstable: wasi.wasiImport
 };
@@ -17,9 +14,5 @@ const importObject = {
 WebAssembly.instantiate(buffer, importObject)
 .then(common.mustCall((results) => {
   assert(results.instance.exports._start);
-  // Currently when running this the exit status of the process will be 71
-  // which if I'm reading the uvwasi error codes correctly matches __WASI_ESRCH
-  // which is "No such process".
-  // TODO(danbev) enable start when the above issue has been sorted out.
-  // WASI.start(results.instance);
+  wasi.start(results.instance);
 }));
