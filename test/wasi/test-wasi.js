c65f453e47607c947d10df252b060fd4595a903a
cjihrig <cjihrig@gmail.com>
2019-09-23 11:20:04 -0400

    wasi: port more wasi libc integration tests
    This commit ports the following tests from
    https://github.com/nodejs/node/pull/27850.
    
    - clock_getres
    - getentropy (currently disabled)
    - getrusage
    - gettimeofday
    - notdir
    - preopen_populates
    - read_file_twice
    - stat
    - stdin
    - write_file
    
    This commit completes the implementations of
    fd_filestat_get() and path_filestat_get(), which
    are needed for the stat test. This commit also
    adds clock_res_get() and partial clock_time_get()
    support.

--- test/wasi/test-wasi.js
+++ test/wasi/test-wasi.js
@@ -3,15 +3,18 @@ require('../common');
 
 if (process.argv[2] === 'wasi-child') {
   const fixtures = require('../common/fixtures');
+  const tmpdir = require('../../test/common/tmpdir');
   const fs = require('fs');
   const path = require('path');
   const { WASI } = require('wasi');
+  tmpdir.refresh();
   const wasmDir = path.join(__dirname, 'wasm');
   const wasi = new WASI({
     args: [],
     env: process.env,
     preopens: {
-      '/sandbox': fixtures.path('wasi')
+      '/sandbox': fixtures.path('wasi'),
+      '/tmp': tmpdir.path
     }
   });
   const importObject = { wasi_unstable: wasi.wasiImport };
@@ -29,12 +32,17 @@ if (process.argv[2] === 'wasi-child') {
 
   function runWASI(options) {
     console.log('executing', options.test);
+    const opts = {};
+
+    if (options.stdin !== undefined)
+      opts.input = options.stdin;
+
     const child = cp.spawnSync(process.execPath, [
       '--experimental-wasm-bigint',
       __filename,
       'wasi-child',
       options.test
-    ]);
+    ], opts);
 
     assert.strictEqual(child.status, options.exitCode || 0);
     assert.strictEqual(child.signal, null);
@@ -42,7 +50,20 @@ if (process.argv[2] === 'wasi-child') {
   }
 
   runWASI({ test: 'cant_dotdot' });
+  runWASI({ test: 'clock_getres' });
   runWASI({ test: 'exitcode', exitCode: 120 });
   runWASI({ test: 'fd_prestat_get_refresh' });
+  // runWASI({ test: 'getentropy' });
+  runWASI({ test: 'getrusage' });
+  runWASI({ test: 'gettimeofday' });
+  runWASI({ test: 'notdir' });
+  runWASI({ test: 'preopen_populates' });
   runWASI({ test: 'read_file', stdout: 'hello from input.txt\n' });
+  runWASI({
+    test: 'read_file_twice',
+    stdout: 'hello from input.txt\nhello from input.txt\n'
+  });
+  runWASI({ test: 'stat' });
+  runWASI({ test: 'stdin', stdin: 'hello world', stdout: 'hello world' });
+  runWASI({ test: 'write_file' });
 }
