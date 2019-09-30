1998e692e6585ddfa1132b8817c6ab28517ba880
cjihrig <cjihrig@gmail.com>
2019-09-30 11:36:00 -0400

    wasi: sync with latest uvwasi
    PR-URL: https://github.com/nodejs/wasi/pull/25
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

--- deps/uvwasi/include/uvwasi.h
+++ deps/uvwasi/include/uvwasi.h
@@ -9,6 +9,19 @@ extern "C" {
 #include "uv_mapping.h"
 #include "fd_table.h"
 
+#define UVWASI_VERSION_MAJOR 0
+#define UVWASI_VERSION_MINOR 0
+#define UVWASI_VERSION_PATCH 1
+#define UVWASI_VERSION_HEX ((UVWASI_VERSION_MAJOR << 16) | \
+                            (UVWASI_VERSION_MINOR <<  8) | \
+                            (UVWASI_VERSION_PATCH))
+#define UVWASI_STRINGIFY(v) UVWASI_STRINGIFY_HELPER(v)
+#define UVWASI_STRINGIFY_HELPER(v) #v
+#define UVWASI_VERSION_STRING UVWASI_STRINGIFY(UVWASI_VERSION_MAJOR) "." \
+                              UVWASI_STRINGIFY(UVWASI_VERSION_MINOR) "." \
+                              UVWASI_STRINGIFY(UVWASI_VERSION_PATCH)
+
+
 typedef struct uvwasi_s {
   struct uvwasi_fd_table_t fds;
   size_t argc;
@@ -51,7 +64,7 @@ uvwasi_errno_t uvwasi_clock_time_get(uvwasi_t* uvwasi,
                                      uvwasi_timestamp_t precision,
                                      uvwasi_timestamp_t* time);
 uvwasi_errno_t uvwasi_environ_get(uvwasi_t* uvwasi,
-                                  char** environ,
+                                  char** environment,
                                   char* environ_buf);
 uvwasi_errno_t uvwasi_environ_sizes_get(uvwasi_t* uvwasi,
                                         size_t* environ_count,
