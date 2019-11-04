738a7fce4ed9323078e882334033dcf3905ecf54
cjihrig <cjihrig@gmail.com>
2019-11-03 20:22:57 -0500

    wasi: refactor flaky tests
    The WASI getrusage() and gettimeofday() tests were flaky because
    an empty for loop was not introducing enough delay on some CI
    machines (the compiler could potentially remove the loop as well).
    With the refactor in this commit, the tests are passing on all
    platforms in the Node CI.

--- test/wasi/c/getrusage.c
+++ test/wasi/c/getrusage.c
@@ -3,22 +3,32 @@
 
 int main() {
   struct rusage ru1;
-  getrusage(RUSAGE_SELF, &ru1);
-
-  for (int i = 0; i < 1000; i++) {
-  }
-
   struct rusage ru2;
-  getrusage(RUSAGE_SELF, &ru2);
+  long long s1;
+  long long us1;
+  long long s2;
+  long long us2;
+  int r;
+  int success = 0;
 
-  // assert that some time has passed
-  long long s1 = ru1.ru_utime.tv_sec;
-  long long us1 = ru1.ru_utime.tv_usec;
-  long long s2 = ru2.ru_utime.tv_sec;
-  long long us2 = ru2.ru_utime.tv_usec;
-  assert(s1 <= s2);
-  if (s1 == s2) {
-    // strictly less than, so the timestamps can't be equal
-    assert(us1 < us2);
+  r = getrusage(RUSAGE_SELF, &ru1);
+  assert(r == 0);
+  s1 = ru1.ru_utime.tv_sec;
+  us1 = ru1.ru_utime.tv_usec;
+
+  for (int i = 0; i < 10000; i++) {
+    r = getrusage(RUSAGE_SELF, &ru2);
+    assert(r == 0);
+    s2 = ru2.ru_utime.tv_sec;
+    us2 = ru2.ru_utime.tv_usec;
+    assert(s1 <= s2);
+
+    // Verify that some time has passed.
+    if (s2 > s1 || (s2 == s1 && us2 > us1)) {
+      success = 1;
+      break;
+    }
   }
+
+  assert(success == 1);
 }
